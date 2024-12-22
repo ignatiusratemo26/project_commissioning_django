098e12f39a5da110ec3f47b8ebf1763173cb08d8
Ignatius Ratemo <ignatiusratemo5@gmail.com>
2024-12-22 08:31:24 +0300

    CORS allow all origins

--- project_commissioning_django/settings.py
+++ project_commissioning_django/settings.py
@@ -79,9 +79,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+# ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
